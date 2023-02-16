19eb7f96ba2013832ba0e5f241148c5b8bfa4d85
longht0911 <hoanglonght0911@gmail.com>
2023-02-16 11:57:57 +0700

    add code 16/2/2023

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-        <p>alo alo</p>
+// function App() {
+//   return (
+//     <div className="App">
+//         <p>alo alo</p>
       
-    </div>
-  );
-}
+//     </div>
+//   );
+// }
 
-export default App;
+// export default App;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { useState } from 'react';
+
+function App()
+{
+  const [count, setCount]=useState(0);
+  function handle()
+  {
+    setCount(function()
+    {
+        return count+1;
+    })
+  }
+  return (
+    <div>
+    <h1>{count||"Number"}</h1>
+    <button onClick={handle}>Click me to plus</button>
+    </div>
+  )
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+root.render(<App />);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
